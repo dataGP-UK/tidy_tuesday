7ee0a6061047d8c1e1bb9fbb3d8822f7c4d2b1f8
dataGP-UK <sihulme21@gmail.com>
2022-09-12 14:51:16 +0100

    initial feature selection and factor created for categorical variable

--- gender_paygap/script/import_cleaning.R
+++ gender_paygap/script/import_cleaning.R
@@ -13,3 +13,49 @@ paygap <-
   read_csv(paste0(
     'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master',
     '/data/2022/2022-06-28/paygap.csv'))
+
+# save raw data to data folder  ** TO DO **
+
+# initial exploration ----
+
+str(paygap)
+head(paygap)
+glimpse(paygap)
+summary(paygap)
+
+# preliminary feature selection
+paygap <- 
+  paygap %>% 
+  select(-c(address, employer_name, company_link_to_gpg_info, 
+            responsible_person))
+
+# manage categorical variable: employer size
+
+# what unique values are there for this variable
+unique(paygap$employer_size)
+
+# formally define missing data: currently "Not provided"
+paygap <- 
+  paygap %>% 
+  mutate(employer_size = na_if(employer_size, 'Not Provided'))
+unique(paygap$employer_size)  # check output
+
+# mutate into ordered factor
+paygap <- 
+  paygap %>%
+  mutate(employer_size =
+           factor(
+             employer_size,
+             ordered = TRUE,
+             levels = c(
+               "250 to 499",
+               "500 to 999",
+               "1000 to 4999",
+               "5000 to 19,999",
+               "20,000 or more"
+             )
+           ))
+
+# check output
+levels(paygap$employer_size)
+str(paygap)
