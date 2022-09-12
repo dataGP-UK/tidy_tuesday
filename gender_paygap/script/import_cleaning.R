73fe40027ebf7566d1c36ea5d4cfc60576e83ea1
dataGP-UK <sihulme21@gmail.com>
2022-09-12 14:27:46 +0100

    data imported

--- gender_paygap/script/import_cleaning.R
+++ gender_paygap/script/import_cleaning.R
@@ -5,3 +5,11 @@
 
 # date created: 12th September 2022
 
+# setup ----
+
+library(tidyverse)
+
+paygap <- 
+  read_csv(paste0(
+    'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master',
+    '/data/2022/2022-06-28/paygap.csv'))
