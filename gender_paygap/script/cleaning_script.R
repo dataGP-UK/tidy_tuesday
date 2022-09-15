db20f6067342330648bb7d870dde9509f77bdedd
dataGP-UK <sihulme21@gmail.com>
2022-09-15 12:45:44 +0100

    source function from file and use to download data

--- gender_paygap/script/cleaning_script.R
+++ gender_paygap/script/cleaning_script.R
@@ -14,24 +14,7 @@ library(here)
 # upload csv files from gov.uk website ----
 
 # using a function
-download_paygap <- 
-    function() {
-        years <- as.character(c(2017, 2018, 2019, 2020, 2021, 2022))
-        url <- "https://gender-pay-gap.service.gov.uk/viewing/download-data/"
-        filepath <- as.character()
-        paygap_raw <- tibble()
-        
-        for (i in years) {
-            filepath[i] <- paste0(url, i)
-        }
-        
-        for (i in 1:length(filepath)) {
-            new_data <- read_csv(filepath[i])
-            paygap_raw <- rbind(paygap_raw, new_data)
-        }
-        paygap_raw
-    }
-
+source("gender_paygap/script/functions/download_paygap.R")
 paygap_raw <- download_paygap()
 
 ## save raw data ----
